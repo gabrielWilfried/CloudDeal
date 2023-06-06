c53a4229f3decbc64af35b9cd61d941ee65b516d
gabrielWilfried <gabrielwilfried0808@gmail.com>
2023-06-06 07:25:05 +0200

    creation of relationships

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -9,4 +9,19 @@ class Payment extends Model
 {
     use HasFactory;
     protected $guarded = [];
+
+    protected $fillable = ['annonce_id', 'amount', 'status'];
+
+
+     // relation MorphMany between payment and files
+     public function files():MorphMany
+     {
+        return $this->morphMany(related: 'App\Http\Models\File', name: 'target');
+     }
+
+     //relation one to many(inverse) between payments and annonces
+     public function annonces():BelongsTo
+     {
+         return $this->belongsTo(Annonce::class);
+     }
 }
--- app/Models/Town.php
+++ app/Models/Town.php
@@ -8,7 +8,30 @@ use Illuminate\Database\Eloquent\Model;
 class Town extends Model
 {
     use HasFactory;
-    protected $guarded = [
+    protected $guarded = ['name'];
+
+    protected $fillable = [
         'name',
+        'region_id',
+        'description'
     ];
+
+
+     // relation MorphMany between towns and files
+     public function files():MorphMany
+     {
+        return $this->morphMany(File::class, 'target');
+     }
+
+     //relation one to many between towns and annonces
+    public function annonces():HasMany
+    {
+        return $this->hasMany(Annonce::class);
+    }
+
+    //relation one to many(inverse) between towns and regions
+    public function regions():BelongsTo
+    {
+        return $this->belongsTo(Region::class);
+    }
 }
