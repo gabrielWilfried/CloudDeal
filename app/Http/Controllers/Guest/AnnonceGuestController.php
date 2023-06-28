84490bb8bd216c6c5d47566686d0184ce1184378
NNJK45 <48721841+NNJK45@users.noreply.github.com>
2023-06-28 14:33:27 +0100

    integration des best produits

--- app/Http/Controllers/Guest/AnnonceGuestController.php
+++ app/Http/Controllers/Guest/AnnonceGuestController.php
@@ -8,18 +8,7 @@ use Illuminate\Http\Request;
 
 class AnnonceGuestController extends Controller
 {
-    public function listAnnonces(Request $request)
-    {
-        $limit = $request->get('limit', 15);
-        $annonces = Annonce::where('is_blocked', false)->paginate($limit);
-        return response()->json($annonces);
-    }
-
-    public function detailsAnnonce(Annonce  $annonce)
-    {
-        $annonce->load('comments', 'category', 'town');
-        return response()->json($annonce);
-    }
+    
     public function BestAnnonce()
     {
         $annonces = Annonce::orderBy('level', 'desc')->take(4)->get();
