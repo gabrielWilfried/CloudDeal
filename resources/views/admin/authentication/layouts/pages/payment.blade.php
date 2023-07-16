1de583bae02ecfd27e97a950021145a9faaa542c
tchamouramses <tchamouramses@gmail.com>
2023-07-16 16:38:38 +0100

    update

--- resources/views/admin/authentication/layouts/pages/payment.blade.php
+++ resources/views/admin/authentication/layouts/pages/payment.blade.php
@@ -1,73 +1,80 @@
 @extends('admin.authentication.layouts.pages.ads.default')
 
 @section('content')
-<div class="col-12">
-    <div class="box">
-        <div class="box-header with-border">
-            <h3 class="box-title">List Payments</h3>
-            <h3 class="box-title text-right mr-35" style="position: absolute; right: 0;">TotalAmount: {{ $montantTotals }}</h3>
-        </div>
-        <!-- /.box-header -->
-        <div class="box-body">
-            <div class="table-responsive">
-                <table id="example6" class="table table-bordered table-striped">
-                    <thead class="bg-info">
-                        <tr>
-                            <th>Nature</th>
-                            <th>Annonce</th>
-                            <th>User</th>
-                            <th>Amount</th>
-                            <th>Status</th>
-                            <th>Payment Date</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($annonces as $annonce)
-                        @if ($annonce->payment)
-                        <tr>
-                            <td>
-                                @if (count($annonce->boosts) > 0)
-                                <span>Boost</span>
-                                @else
-                                <span>Taxe</span>
+    <div class="col-12">
+        <div class="box">
+            <div class="box-header with-border">
+                <h3 class="box-title">List Payments</h3>
+                <h3 class="box-title text-right mr-35" style="position: absolute; right: 0;">TotalAmount: {{ $montantTotals }}
+                </h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+                <div class="table-responsive">
+                    <table id="example6" class="table table-bordered table-striped">
+                        <thead class="bg-info">
+                            <tr>
+                                <th>Nature</th>
+                                <th>Annonce</th>
+                                <th>User</th>
+                                <th>Amount</th>
+                                <th>Status</th>
+                                <th>Payment Date</th>
+                                @if (auth()->user()->is_admin)
+                                    <th>Actions</th>
                                 @endif
-                            </td>
-                            <td>{{ $annonce->name }}</td>
-                            <td>{{ $annonce->user->name }}</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($annonces as $annonce)
+                                @if ($annonce->payment)
+                                    <tr>
+                                        <td>
+                                            @if (count($annonce->boosts) > 0)
+                                                <span>Boost</span>
+                                            @else
+                                                <span>Taxe</span>
+                                            @endif
+                                        </td>
+                                        <td>{{ $annonce->name }}</td>
+                                        <td>{{ $annonce->user->name }}</td>
 
-                            <td>{{ toMoney($annonce->payment->amount) }}</td>
-                            @if ($annonce->payment->status === 'PENDING')
-                            <td class="text-info text-bold">{{ $annonce->payment->status }}</td>
-                            @elseif ($annonce->payment->status === 'APPROVED')
-                            <td class="text-success text-bold">{{ $annonce->payment->status }}</td>
-                            @else
-                            <td class="text-danger  text-bold">{{ $annonce->payment->status }}</td>
-                            @endif
-                            <td>{{ $annonce->created_at }}</td>
-                            <td>
-                                <!-- Buttons -->
-                                @if ($annonce->payment->status === 'PENDING')
-                                <form action="{{ route('admin.payments.approve', ['annonce' => $annonce]) }}"
-                                    method="get">
-                                    <button type="submit" class="btn btn-success btn-sm mb-5"><i
-                                            class="fa fa-check "></i></button>
+                                        <td>{{ toMoney($annonce->payment->amount) }}</td>
+                                        @if ($annonce->payment->status === 'PENDING')
+                                            <td class="text-info text-bold">{{ $annonce->payment->status }}</td>
+                                        @elseif ($annonce->payment->status === 'APPROVED')
+                                            <td class="text-success text-bold">{{ $annonce->payment->status }}</td>
+                                        @else
+                                            <td class="text-danger  text-bold">{{ $annonce->payment->status }}</td>
+                                        @endif
+                                        <td>{{ $annonce->created_at }}</td>
+                                        @if (auth()->user()->is_admin)
+                                            <td>
+                                                <!-- Buttons -->
+                                                @if ($annonce->payment->status === 'PENDING')
+                                                    <form
+                                                        action="{{ route('admin.payments.approve', ['annonce' => $annonce]) }}"
+                                                        method="get">
+                                                        <button type="submit" class="btn btn-success btn-sm mb-5"><i
+                                                                class="fa fa-check "></i></button>
 
-                                </form>
-                                <form action="{{ route('admin.payments.cancel', ['annonce' => $annonce]) }}"
-                                    method="get"><button class="btn btn-danger btn-sm  mb-5">
-                                        <i class="fa fa-times "></i>
-                                    </button>
-                                </form>
+                                                    </form>
+                                                    <form
+                                                        action="{{ route('admin.payments.cancel', ['annonce' => $annonce]) }}"
+                                                        method="get"><button class="btn btn-danger btn-sm  mb-5">
+                                                            <i class="fa fa-times "></i>
+                                                        </button>
+                                                    </form>
+                                                @endif
+                                            </td>
+                                        @endif
+                                    </tr>
                                 @endif
-                            </td>
-                        </tr>
-                        @endif
-                        @endforeach
-                    </tbody>
-                </table>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
